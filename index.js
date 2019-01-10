44eb182557a071f018cd97f6ba1fefa4ce744df6
ankush29 <saini.ankush29@gmail.com>
2019-01-10 13:33:17 +0530

    static

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HELLO!')
-})
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+app.use(express.static('public'))
+
+app.listen(8080, () => console.log('Server running on port 8080'))
