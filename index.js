0f1a3757d41a205b2d89f494277c7bb6491dda6d
ankush29 <saini.ankush29@gmail.com>
2019-01-11 22:52:33 +0530

    remove static

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
-
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'build')));
